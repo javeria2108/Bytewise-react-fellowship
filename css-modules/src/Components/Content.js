6b96e6252040c9bbf4a45fc446a06c67053e582d
javeria2108 <javeriazh@gmail.com>
2023-05-31 12:49:23 +0500

    adding css-modules

---
+++ css-modules/src/Components/Content.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ContentCSS from "./Content.module.css";
+export default function Content(){
+    return (
+    <p className={ContentCSS.p}>
+        This is content style
+    </p>
+    )
+}
---
+++ css-modules/src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import HeaderCSS from "./Header.module.css";
+export default function Header(){
+return(
+   <h1 className={HeaderCSS.h1}>
+    This is Header Style
+   </h1>
+)
+}
